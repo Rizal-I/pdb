4f661e953369de3f2f200186ee6ff083f8a523a8
Rizal-I <rizal2266@gmail.com>
2023-12-05 03:37:32 +0000

    rollback new

--- rollback-pdb.sh
+++ rollback-pdb.sh
@@ -1,11 +1,2 @@
-namespaces=$(kubectl get namespaces -o=json | jq -r '.items[].metadata.name')
-for namespace in $namespaces; do
-    pdb_info=$(kubectl get poddisruptionbudget -n "$namespace" -o=json | jq -r '.items[0] | select(.status.currentHealthy > 5) | {name: .metadata.name, namespace: .metadata.namespace}')
-    if [ -n "$pdb_info" ]; then
-        pdb_name=$(echo "$pdb_info" | jq -r '.name')
-        pdb_namespace=$(echo "$pdb_info" | jq -r '.namespace')
-        # convert to minAvailable
-        kubectl patch poddisruptionbudget $pdb_name -n $pdb_namespace --type='merge' -p '{"spec": {"minAvailable": 2, "maxUnavailable": null}}'
-        echo "pdb $pdb_name in namespace $pdb_namespace rolled back" 
-    fi
-done
+kubectl delete -f pdb-dumped/
+kubectl apply -f pdb-dumped/
