bf4d0c22e9de445d84c11302696de97172af0182
Rizal-I <69077191+Rizal-I@users.noreply.github.com>
2023-12-27 10:24:49 +0700

    Update delete-pdb.sh

--- delete-pdb.sh
+++ delete-pdb.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
-# Read PDB names from the file
-PDB_NAMES_FILE="pdb_names.txt"
-PDB_NAMES=($(cat $PDB_NAMES_FILE))
-
-# Iterate over each PDB and delete it
-for PDB_NAME in "${PDB_NAMES[@]}"; do
+# Iterate over each generated PDB and delete it
+for ns in "$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')"; do
   # Extract the namespace and PDB name
-  NAMESPACE=$(echo "$PDB_NAME" | cut -d'/' -f1)
-  PDB=$(echo "$PDB_NAME" | cut -d'/' -f2)
+  kubectl config set-context --current --namespace=$ns
+  NAMESPACE=$(kubectl get pdb -l generated=true -ojson | jq -r '.items[] | "\(.metadata.namespace)"')
+  PDB=$(kubectl get pdb -l generated=fisclouds -ojson | jq -r '.items[] | "\(.metadata.name)"')
 
   kubectl delete pdb -n $NAMESPACE $PDB
   echo "PodDisruptionBudget $PDB deleted in namespace $NAMESPACE"
 done
-rm pdb_names.txt
-echo "pdb_names.txt" deleted.
